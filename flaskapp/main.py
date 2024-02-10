63455227a332be4f663a0600b0ca770b19b3e870
EC2 Default User <ec2-user@ip-172-31-3-157.us-west-2.compute.internal>
2024-02-10 19:15:07 +0000

    add flask

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from flask import json
+from utils import welcome_message
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return welcome_message()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
